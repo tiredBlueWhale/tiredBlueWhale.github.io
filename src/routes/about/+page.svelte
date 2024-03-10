570919f560ca18efbbc6da0698e3a08115b64a2b
tiredBlueWhale <tired.blue.whale@gmail.com>
2024-03-10 15:52:04 +0200

    Add about text

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -5,8 +5,8 @@
 <SectionScroll title="About" color="bg-blue-700">
 	<h1 class="text-xl lg:text-5xl font-martian font-bold">Tired Blue Whale</h1>
 	<h2>
-		A indie developer who likes to spend too much time on the small little (unimportant) details ...
-		for example not his website.
+		A indie developer who likes to spend too much time on the small (unimportant) details ... and
+		tries to create little escapes to disconnect for a while.
 	</h2>
 	<div class="flex-1" />
 </SectionScroll>
