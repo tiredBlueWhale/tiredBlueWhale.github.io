41aec98e00b07c7c34f92ae334c0d6a2eba8d528
tiredBlueWhale <tired.blue.whale@gmail.com>
2024-03-31 13:53:34 +0300

    Maybe fixed iOS one line

--- src/routes/playground/+page.svelte
+++ src/routes/playground/+page.svelte
@@ -1,3 +1,5 @@
+<!-- https://localhost:5173/playground?title=Looking for&[Human, Amazing ideas, Developer, Talent, Telepath, Psychic, an undeniable amazing taste in music]&readingSpeed=1.4&phone=3584578387387 -->
+
 <script>
 	import { onMount } from 'svelte';
 	import { page } from '$app/stores';
@@ -13,6 +15,8 @@
 	let content = [];
 	/** @type {number} */
 	let readingSpeed;
+	/** @type {boolean} */
+	let isMultiline = false;
 	/** @type {string} */
 	let phone;
 
@@ -36,32 +40,38 @@
 			numberOfWords += entry.split(' ').length;
 		}
 
-		readingSpeed = parseFloat($page.url.searchParams.get('readingSpeed') || '1.2');
-		scrollContainer.animate([{ transform: 'translateX(0%)' }, { transform: 'translateX(-50%)' }], {
+		readingSpeed = parseFloat($page.url.searchParams.get('readingSpeed') || '0.8');
+		scrollContainer.animate([{ transform: 'translateX(0)' }, { transform: 'translateX(-50%)' }], {
 			duration: numberOfWords * readingSpeed * 1000,
 			easing: 'linear',
 			iterations: Infinity
 		});
-		content = [...content, ...content];
+		content = [...content, ...content, ...content, ...content];
+
+		isMultiline = 'true' === ($page.url.searchParams.get('isMultiline') || 'false');
 
 		phone = $page.url.searchParams.get('phone') || '';
 	});
 </script>
 
-<section class="h-screen flex flex-col justify-center items-center bg-gray-900">
+<section class="h-screen flex flex-col justify-center bg-gray-900">
 	<h1 class="pb-4 text-6xl md:text-7xl lg:text-9xl font-martian font-bold text-center">
 		{title}
 	</h1>
-	<div bind:this={scrollContainer} class="pb-4 flex flex-row font-press-start-2p">
-		{#each content as entry}
-			<div
-				class="px-8 text-nowrap text-center text-2xl md:text-4xl lg:text-7xl text-red-600 text-shadow-neon shadow-red-700"
-			>
-				{entry}
-			</div>
-		{/each}
+	<div class="w-full overflow-x-hidden">
+		<div
+			bind:this={scrollContainer}
+			class="flex flex-row items-center {isMultiline ? 'w-min' : 'w-max'} "
+		>
+			{#each content as entry}
+				<div
+					class="px-8 md:px-12 lg:px-16 font-press-start-2p text-2xl md:text-4xl lg:text-7xl text-red-600 text-shadow-neon shadow-red-700"
+				>
+					{entry}
+				</div>
+			{/each}
+		</div>
 	</div>
-
 	{#if phone}
 		<a href="tel:+{phone}" class="flex flex-col justify-center items-center">
 			<h3 class="text-3xl lg:text-7xl">Call: +{phone}</h3>
