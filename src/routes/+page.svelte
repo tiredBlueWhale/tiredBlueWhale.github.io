1087107353fcccd2286aab236fe30607417ab2fe
tiredBlueWhale <tired.blue.whale@gmail.com>
2024-04-08 12:49:53 +0300

    Add whatsapp sticker pack

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -23,17 +23,23 @@
 </Section>
 <Section sectionId="random" zIndex="z-[6]" bgColor="bg-slate-950">
 	<div class="h-full flex flex-col items-stretch py-4 px-6 md:py-32 md:px-16 xl:px-52">
-		<h1 class="text-4xl md:text-6xl lg:text-8xl font-bold">Random</h1>
-		<div class="flex-1 flex flex-col justify-center items-stretch">
+		<div
+			class="flex-1 flex flex-col justify-center items-stretch text-xl md:text3xl lg:text-5xl text-slate-300/90 *:border-y-2 *:p-4 *:border-slate-300/50 *: *:transition-transform *:"
+		>
 			<a
 				href="https://apps.apple.com/mx/app/tired-blue-whale/id6480527807"
 				target="_blank"
 				rel="noopener noreferrer"
-				class="no-underline flex justify-center items-center border-2 rounded-lg p-4 border-slate-300/50 hover:border-slate-300 transition-transform hover:scale-105"
+				class="no-underline flex justify-center items-center hover:border-slate-300 hover:scale-110"
 			>
-				<h1 class="text-xl md:text3xl lg:text-5xl text-slate-300/90 hover:text-slate-100">
-					Tired Blue Whale iMessage Sticker
-				</h1>
+				<h1 class="hover:text-slate-100">Tired Blue Whale iMessage Sticker</h1>
+			</a>
+			<a
+				href="tired_blue_whale.wastickers"
+				download=""
+				class="no-underline flex flex-col justify-center items-center hover:border-slate-300 hover:scale-110"
+			>
+				<h1 class="hover:text-slate-100">Tired Blue Whale Whatsapp Sticker</h1>
 			</a>
 		</div>
 	</div>
